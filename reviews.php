aed2ec22c4d99277f4fe19e4dd4aab643c6b0ec2
Javier Carranza <57335448+JavierACarranza@users.noreply.github.com>
2023-04-23 20:39:29 -0700

    updated reviews

--- reviews.php
+++ reviews.php
@@ -12,13 +12,6 @@
     <div class="container">
 
 <form method="post" action="submit-review.php">
-  <label for="item_id">Select an item:</label>
-  <select name="item_id" id="item_id">
-    <option value="1">Item 1</option>
-    <option value="2">Item 2</option>
-    <option value="3">Item 3</option>
-  </select>
-  <br>
   <label for="rating">Rating:</label>
   <select name="rating" id="rating">
     <option value="excellent">Excellent</option>
